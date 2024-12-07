0f7a242009cd93dd12e678b844c3b5d808d4fe2d
Gulilil <juan.csantoso@gmail.com>
2024-12-07 17:56:13 +0700

    update gui

--- main.py
+++ main.py
@@ -23,15 +23,21 @@ def select_input_image():
         filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")]
     )
     filename = file_path.split("/")[-1]
+
     if file_path:
         # Display the selected file path
-        selected_image_label.config(text=f"{filename}", fg="blue")
+        selected_image_name.config(text=f"{filename}", fg="blue")
         
-        # Load and display the image
+         # Load and display the image
         img = Image.open(file_path)
+        img = img.resize((480,270))
         img_tk = ImageTk.PhotoImage(img)
-        image_input.config(image=img_tk)
-        image_input.image = img_tk  # Keep a reference to avoid garbage collection
+
+        image_display_input_img.config(image=img_tk)
+        image_display_input_img.image = img_tk  # Keep a reference to avoid garbage collection
+        
+    else:
+        selected_image_name.config(text="No Selected Image", fg='red')
 
 
 
@@ -58,14 +64,18 @@ if __name__ == "__main__":
   image_input_grid.grid(row=1, column=0, sticky='nsew')
   configure_grid(image_input_grid, 2, 2)
   # Top Left: Label
-  image_input_label = tk.Label(image_input_grid, text="Input Image", font=("Helvetica", 12), anchor="w")
+  image_input_label = tk.Label(image_input_grid, text="Input Image:", font=("Helvetica", 12), anchor="w")
   image_input_label.grid(row=0, column=0)
   # Top Right : Button
   button = tk.Button(image_input_grid, text="Select Image", command=select_input_image, font=("Helvetica", 12), anchor="w")
   button.grid(row=0, column=1)
   # Bottom Left: Selected Label
-  selected_image_label = tk.Label(image_input_grid, text="No Image Selected", font=("Helvetica", 12), fg="red", anchor="w")
+  selected_image_label = tk.Label(image_input_grid, text="Selected Image:", font=("Helvetica", 12), anchor="w")
   selected_image_label.grid(row=1, column=0)
+  # Bottom Right: Selected Name
+  selected_image_name = tk.Label(image_input_grid, text="No Selected Image", font=("Helvetica", 12), fg="red", anchor="w")
+  selected_image_name.grid(row=1, column=1)
+
 
 
 
@@ -75,8 +85,21 @@ if __name__ == "__main__":
   img_display_grid.grid(row=0, column=1, sticky='nsew')
   configure_grid(img_display_grid, 2, 1)
 
-  # Add a label to display the image
-  image_input = tk.Label(root)
+  # Display Input
+  image_display_input_grid = tk.Frame(img_display_grid)
+  image_display_input_grid.grid(row=0, column=0)
+  configure_grid(image_display_input_grid, 2, 1, [4, 1], None)
+  image_display_input_label = tk.Label(image_display_input_grid, text="Input Image Display", font=("Helvetica", 12), anchor="w")
+  image_display_input_label.grid(row=0, column=0)
+  image_display_input_img = tk.Label(image_display_input_grid)
+  image_display_input_img.grid(row=1, column=0)
+
+
+  
+
+  # Display Result
+  image_display_result_grid = tk.Frame(img_display_grid)
+  image_display_result_grid.grid(row=1, column=0)
 
   # Run the application
   root.mainloop()
